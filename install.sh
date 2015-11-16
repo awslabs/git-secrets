d6942be5366ff83803d7a06a57170efcb8fe9288
Michael Dowling <mtdowling@gmail.com>
2015-11-15 19:43:16 -0800

    Cleaning up installer script

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@
 # If no directory is passed, then the script will install the git subcommand to
 # /usr/local/bin (if available) or as a last resort, git --exec-path.
 
-INSTALL_DIR="$1"
-
 # Show help if "-h" is provided.
-if [ "$INSTALL_DIR" == "-h" ]; then
+if [ "$1" == "-h" ]; then
   echo "usage: install.sh [<path>]"
   echo
   echo "Installs the git secrets command and sets up your environment."
@@ -25,54 +23,58 @@ if [ "$INSTALL_DIR" == "-h" ]; then
   exit 0
 fi
 
-pass() { echo -e " $(tput setaf 2)✓$(tput sgr 0) $1"; }
-warn() { echo -e " $(tput setaf 3)-$(tput sgr 0)" $1; }
-fail() { echo -e " $(tput setaf 1)✗$(tput sgr 0)" $1 && exit 1; }
-
-# Installs git-secrets to an approriate path.
-install_secrets() {
-  if [ ! -z "${INSTALL_DIR}" ]; then
-    # The user may pass in a custom installation directory.
-    [ ! -d "${INSTALL_DIR}" ] && fail "${INSTALL_DIR} does not exist"
-  elif [ -d "/usr/local/bin" ]; then
-    # Use /usr/local/bin as the directory if it exists.
-    INSTALL_DIR="/usr/local/bin"
-  else
-    # Fall back to using git --exec-path
-    INSTALL_DIR="$(git --exec-path)"
-  fi
-  path="${INSTALL_DIR}/git-secrets"
-  cp git-secrets "${path}" && chmod +x "${path}" \
-    && pass "Installed git-secrets command at ${INSTALL_DIR}/git-secrets" \
-    || fail "Could not install git-secrets at ${INSTALL_DIR}/git-secrets"
+pass() {
+  echo -e " $(tput setaf 2)✓$(tput sgr 0) $1";
 }
 
-# Determine the most appropriate grep for the system. gegrep is often used
-# for extended greps to not overwrite system grep (e.g., homebrew).
-set_grep_config() {
-  local grep_command="$(git config --get secrets.grep)"
-  if [ ! -z "${grep_command}" ]; then
-    pass "grep command already configured: ${grep_command}"
-    return
-  elif [ -x "$(which gegrep 2>&1)" ]; then
-    grep_command='gegrep'
-  elif [ -x "$(which egrep 2>&1)" ]; then
-    grep_command='egrep'
-  else
-    fail "Could not find grep on your system"
-  fi
-  git config --global --add secrets.grep "${grep_command}"
-  pass "Configured grep command: ${grep_command}"
+fail() {
+  echo -e " $(tput setaf 1)✗$(tput sgr 0) $1"
+  exit 1
 }
 
-success_check() {
-  git secrets -h > /dev/null 2>&1 \
-    && pass "git-secrets has been installed successfully\n" \
-    || fail "git-secrets did not install correctly\n"
+set_grep_command() {
+  git config --global --add secrets.grep "$1"
+  pass "Configured grep command: $1"
 }
 
 echo -e "Installing git-secrets...\n"
 
-install_secrets
-set_grep_config
-success_check
+# Determine the appropriate directory to install
+########################################################
+install_dir="$1"
+if [ ! -z "$install_dir" ]; then
+  # The user may pass in a custom installation directory.
+  [ ! -d "${install_dir}" ] && fail "${install_dir} does not exist"
+elif [ -d "/usr/local/bin" ]; then
+  # Use /usr/local/bin as the directory if it exists.
+  install_dir="/usr/local/bin"
+else
+  # Fall back to using git --exec-path
+  install_dir="$(git --exec-path)"
+fi
+
+# Installs git-secrets to an approriate path.
+########################################################
+path="${install_dir}/git-secrets"
+cp git-secrets "${path}" && chmod +x "${path}" \
+  && pass "Installed git-secrets command at ${install_dir}/git-secrets" \
+  || fail "Could not install git-secrets at ${install_dir}/git-secrets"
+
+# Determine the most appropriate grep for the system. gegrep is sometimes
+# used for extended greps to not overwrite system grep (e.g., homebrew).
+########################################################
+if [ ! -z "$(git config --get secrets.grep)" ]; then
+  pass "grep command already configured"
+elif [ -x "$(which gegrep 2>&1)" ]; then
+  set_grep_command 'gegrep'
+elif [ -x "$(which egrep 2>&1)" ]; then
+  set_grep_command 'egrep'
+else
+  fail "Could not find grep on your system"
+fi
+
+# Last step: call help to ensure it installed correctly
+########################################################
+git secrets -h > /dev/null 2>&1 \
+  && pass "git-secrets has been installed successfully\n" \
+  || fail "git-secrets did not install correctly\n"
